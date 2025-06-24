2d7868d4af053d55f36cb5776992d01cf0cc419f
Tyler Crimando <129411200+tyler-cr@users.noreply.github.com>
2025-06-24 13:20:46 -0700

    Create structs.h

---
+++ structs.h
@@ -0,0 +1,9 @@
+typedef struct {
+  const char* language;
+  const char* fileExtension;
+  const char* singleComment;
+  const char* multiComment;
+} fileTypeDictElement;
+
+char* getSingleComment(char* langOrExtension);
+char* getMultiComment(char* langOrExtension);
