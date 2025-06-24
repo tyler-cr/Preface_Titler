d7e206d5a2131ed40230aec9aae81c74e66d54dd
Tyler Crimando <129411200+tyler-cr@users.noreply.github.com>
2025-06-24 13:58:57 -0700

    Create structs.c

---
+++ structs.c
@@ -0,0 +1,30 @@
+#include "structs.h"
+
+#define PLACEHOLDER 20
+fileTypeDictElement langList[PLACEHOLDER];
+
+fileTypeDictElement* findLanguage(char* langOrExtension){
+  for (int i = 0; i < PLACEHOLDER; i++){
+    if (strcmp(langList[i].language, langOrExtension) == 0 || 
+        strcmp(langList[i].fileExtension, langOrExtension) == 0){
+      return &langList[i];
+    }
+  }
+  return NULL;
+}
+
+char* getSingleComment(char* langOrExtension){
+  fileTypeDictElement* language = findLanguage(langOrExtension);
+  if (language) return language->singleComment;
+
+  return "Language or extension not found!\n Please check spelling "
+         "or list of languages currently supported in structs.c";
+}
+
+char* getMultiComment(char* langOrExtension){
+  fileTypeDictElement* language = findLanguage(langOrExtension);
+  if (language) return language->multiComment;
+
+  return "Language or extension not found!\n Please check spelling "
+         "or list of languages currently supported in structs.c";
+}
